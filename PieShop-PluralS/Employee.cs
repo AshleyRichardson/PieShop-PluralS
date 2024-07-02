c6489736c0042cf58d46410fd308151f91d7c4a9
Ashley <19aar94@gmail.com>
2024-07-01 22:21:59 -0500

    "PluralS Classes, Enums and struts"

---
+++ PieShop-PluralS/Employee.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop_PluralS
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        public EmployeeType employeeType;
+        
+        //constructors set the values of fields from above
+        public Employee(string first, string last, string em, DateTime bd): this(first, last, em, bd, 0, EmployeeType.StoreManager)//uses a default for enum which is Store Manager
+        {
+
+        }
+        public Employee(string first, string last, string em, DateTime bd, double rate, EmployeeType empType) 
+        {
+            firstName = first;
+            lastName = last;
+            email = em;
+            birthDay = bd;
+            hourlyRate = rate;
+            employeeType = empType;
+        } 
+
+        public void PerformWork()
+        {
+            //numOfHoursWorked++;
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numOfHoursWorked} hour(s)!");
+
+            //invoke bottom method inside this method
+            PerformWork(minimalHoursWorkedUnit);
+        }
+
+        public void PerformWork(int numOfHours)
+        {
+            numOfHoursWorked += numOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numOfHours} hour(s)!");
+        }
+
+        public int CalculateBonus(int bonus)
+        {
+            if (numOfHoursWorked > 10)
+                bonus *= 2;
+
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;
+        }
+
+
+        //using out keyword
+        //same as ref but argument does not need to be initialized in caller i.e bonusTax
+        //parameter must be assigned before you exit the method
+        public int CalculateBonusAndBonusTax(int bonus, out int bonusTax)
+        {
+
+            bonusTax = 0; 
+            if (numOfHoursWorked > 10)
+                bonus *= 2;
+
+            if (bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }
+
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+
+            return bonus;
+        }
+
+        /*  public int CalculateBonusAndBonusTax(int bonus, ref int bonusTax)
+          {
+              if (numOfHoursWorked > 10)
+                  bonus *= 2;
+
+              if(bonus >= 200)
+               {
+                  bonusTax = bonus / 10;
+                  bonus -= bonusTax;
+              }
+
+              Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+
+              return bonusTax;
+          }
+        */
+
+        //using a list of strings
+        //practicing w/Using Statements as well
+        public static void UsingACustomType()
+        {
+            List<string> list = new List<string>();
+            StringBuilder sb = new StringBuilder();
+        }
+
+        //means converting current object to JSON string and passing it to SerializeObject method
+        //need to install Neton.JSOFT
+        /*public string ConvertToJson()
+        {
+            //"this" references current object
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+        */
+
+
+        public double ReceiveWage(bool resetHours = true)
+        {
+           if(employeeType == EmployeeType.Manager)
+            {
+                Console.WriteLine($"An extra was added to the wage since {firstName} is a manager!");
+                wage = numOfHoursWorked * hourlyRate * 1.25;
+            }
+            else
+            {
+                wage = numOfHoursWorked * hourlyRate;
+            }
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numOfHoursWorked} hour(s) of work.");
+
+            if (resetHours)
+                numOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        /* public double ReceiveWage(bool resetHours = true)
+         {
+             wage = numOfHoursWorked * hourlyRate;
+
+             Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numOfHoursWorked} hour(s) of work.");
+
+             if(resetHours)
+                 numOfHoursWorked = 0;
+
+             return wage;
+         }
+        */
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail:\t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+        }
+    }
+}
---
+++ PieShop-PluralS/Program.cs
@@ -0,0 +1,139 @@
+﻿
+
+
+using PieShop_PluralS;
+using System.Text;
+
+//Value Types
+/*int a = 42;
+int aCopy = a;
+Console.WriteLine($"Value of a : {a} and value of copy of a :{aCopy}");
+aCopy = 100;
+*/
+
+//Reference Types
+Console.WriteLine("Creating an employee");
+Console.WriteLine("-----------------------");
+
+//initially throws error because we need employee type. The constructor has changed
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25, EmployeeType.Manager);//added type Manager
+
+Employee testEmployee = bethany;
+testEmployee.firstName = "Ashley";
+testEmployee.DisplayEmployeeDetails();
+bethany.DisplayEmployeeDetails();
+
+//using structs
+WorkTask task;
+task.description = "Bake delicious Pies";
+task.hours = 3;
+task.PerformWorkTask();
+
+//need to install Newton.jsoft
+//string bethanyAsJson = bethany.ConvertToJson();
+//Console.WriteLine(bethanyAsJson);
+
+//bethany.PerformWork(25);
+
+
+/*
+* using reference to invoke change on the ORIGINAL value of the variable 
+int minimumBonus = 100;
+int bonusTax = 0;
+int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus , ref bonusTax);
+Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
+*/
+
+/*using out keyword
+int minimumBonus = 100;
+int bonusTax = 0;
+int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, out bonusTax);
+Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
+*/
+/*
+ Console.WriteLine("Creating an employee");
+Console.WriteLine("-----------------------");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979,1,16), 25);
+//can also use var 
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(12);
+bethany.PerformWork();
+
+
+bethany.firstName = "John";
+bethany.hourlyRate = 10;
+
+//can directly change info about specific object
+//overloading 
+
+
+double receivedWageBethany = bethany.ReceiveWage(true);
+Console.WriteLine($"Wage paid(message from program): { receivedWageBethany}");
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("-----------------------");
+
+var george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+//can also use var 
+
+george.DisplayEmployeeDetails();
+george.PerformWork();
+george.PerformWork(3);
+george.PerformWork();
+george.PerformWork();
+george.PerformWork(8);
+
+var receivedWageGeorge = george.ReceiveWage(true);
+*/
+
+
+/*
+//string immutability
+string name = "bethany;";
+string anotherName = name;
+name += "smith";
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Another name: " + anotherName);
+
+string upperCaseName = name.ToUpper();
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Uppercase name: " + upperCaseName);
+
+//////////////////////////////////////////////////////////////
+
+//creating 2500 copies in memory; opportunity to use StringBuilder
+string indexes = string.Empty;
+
+for (int i = 0; i < 2500; i++)
+{
+    indexes += i.ToString();
+}
+
+//how to use StringBuilder
+string firstName = "Bethany";
+string lastName = "Smith";
+
+StringBuilder sb = new StringBuilder();
+
+sb.Append("Last name: ");
+sb.AppendLine(lastName);
+sb.Append("First Name: ");
+sb.Append(firstName);
+
+string result = sb.ToString();
+
+//using it with loop
+StringBuilder sb2 = new StringBuilder();
+for (int i = 0; i < 2500; i++)
+{
+    sb.Append(i);
+    sb.Append(" ");
+}
+*/
