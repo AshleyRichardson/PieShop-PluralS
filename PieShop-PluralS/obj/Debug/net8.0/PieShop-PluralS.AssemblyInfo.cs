214a74940f67ee54f22bb7a51195db6b2bf0de32
Ashley <19aar94@gmail.com>
2024-10-06 19:15:35 -0500

    10/6 Working with arrays

--- PieShop-PluralS/obj/Debug/net8.0/PieShop-PluralS.AssemblyInfo.cs
+++ PieShop-PluralS/obj/Debug/net8.0/PieShop-PluralS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PieShop-PluralS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6489736c0042cf58d46410fd308151f91d7c4a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PieShop-PluralS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PieShop-PluralS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
