eca6344a106e79be4693821b402d7a4a15e985a3
Ashley <19aar94@gmail.com>
2024-10-06 19:13:27 -0500

    10/6 arrays

---
+++ PieShop-PluralS/accounting/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop_PluralS.Accounting
+{
+
+    public record Account (string AccountNumber);
+    //positional record
+
+
+
+
+   /* public record Account
+           //word changes, but same concept.
+           //records used when there is just data, typically same as classes but classes contain otheer data members
+    {
+        private string accountNumber;
+
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+            set
+            {
+                accountNumber = value;
+
+            }
+
+        }
+    }
+   */
+
+
+    /*  public class Account
+      {
+          private string accountNumber;
+
+          public string AccountNumber
+          {
+              get { return accountNumber; }
+              set
+              {
+                  accountNumber = value;
+
+              }
+
+          }
+      }
+    */
+    //can be made into a record
+}
